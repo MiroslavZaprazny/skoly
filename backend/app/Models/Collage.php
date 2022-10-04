04b1a67a59519b56b2b2fc382fe7596327a6f6ec
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2022-10-04 20:51:01 +0200

    Migrations, relationships

---
+++ backend/app/Models/Collage.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class Collage extends Model
+{
+    use HasFactory;
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
