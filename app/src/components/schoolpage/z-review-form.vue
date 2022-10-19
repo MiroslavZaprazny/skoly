fcaee28efacab0e7c0a864ad7be38e625a4368cf
mihi23 <60383878+mihi23@users.noreply.github.com>
2022-10-19 23:47:09 +0200

    review display + review form (wip)

---
+++ app/src/components/schoolpage/z-review-form.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col text-center">
+    <h1>Podelte sa o svoj nazor</h1>
+    <div class="w-full flex justify-center">
+      <input class="w-1/3 border-2 border-purple-primary focus:outline-none text-purple-primary focus:border-purple-500 focus:ring-border-pink-500 focus:ring-1 rounded-lg p-2" placeholder="Ja si o tejto skole myslím..." type="text">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- app/src/pages/schoolpage/schoolpage.vue
+++ app/src/pages/schoolpage/schoolpage.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-    <p>{{ id }}</p>
-    <p>{{schoolItem.name}}</p>
+    <div class="border-b border-purple-primary mx-10 mb-20">
+      <p>{{schoolItem.name}}</p>
+      <p>{{schoolItem.ratings[0].body}}</p>
+    </div>
+    <div class="mx-10">
+      <z-review-form></z-review-form>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import zReviewForm from '../../components/schoolpage/z-review-form.vue';
 
 export default {
+  components: { zReviewForm },
   props: {
     id: String,
   },
@@ -19,10 +26,9 @@ export default {
   },
   methods: {
     async fetchSchool() {
-      console.log(this.id);
       await axios.get(`http://127.0.0.1:8000/api/collage/${this.id}`).then((response) => {
         this.schoolItem = response.data.data;
-        console.log(this.schoolItem);
+        // console.log(this.schoolItem);
       });
     },
   },
