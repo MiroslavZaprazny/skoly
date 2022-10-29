202e4070445a6b0e9a89b942e64e0945c3bee9d1
mihi23 <60383878+mihi23@users.noreply.github.com>
2022-10-29 14:55:11 +0200

    added lib for star rating

--- app/src/components/schoolpage/z-review-form.vue
+++ app/src/components/schoolpage/z-review-form.vue
@@ -3,13 +3,24 @@
     <h1>Podelte sa o svoj nazor</h1>
     <div class="w-full flex justify-center">
       <input class="w-1/3 border-2 border-purple-primary focus:outline-none text-purple-primary focus:border-purple-500 focus:ring-border-pink-500 focus:ring-1 rounded-lg p-2" placeholder="Ja si o tejto skole myslím..." type="text">
+      <star-rating v-model="rating" :star-size="40"></star-rating>
+      <button>Odoslať</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import StarRating from 'vue-star-rating'
 
+export default {
+  data() {
+    return {
+      rating: '',
+    }
+  },
+  components: {
+    StarRating
+  }
 }
 </script>
 
