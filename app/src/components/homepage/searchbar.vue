d914b553bd4bd8142dff74399f16a0d564513d93
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2022-12-11 12:36:35 +0100

    update search debounce, check search length

--- app/src/components/homepage/searchbar.vue
+++ app/src/components/homepage/searchbar.vue
@@ -1,11 +1,14 @@
 <template>
-    <div class="bg-gray-50 text-center py-14">
-        <h1 class="text-purple-primary text-3xl font-bold mb-2">Nájdi si tu pravú vysokú školu pre teba</h1>
-        <h2 class="text-purple-primary text-lg font-semibold">Hľadaj spomedzi všektých vysokých škôl na Slovensku</h2>
-        <div class="mt-20">
-          <input class="border-2 border-purple-primary focus:outline-none text-purple-primary focus:border-purple-500 focus:ring-border-pink-500 focus:ring-1 rounded-lg p-2 w-1/4" type="text" placeholder="FEI STU" v-model="search" v-debounce:150="searchUpdate" debounce-evets="['click', 'keyup']">
-        </div>
+  <div class="bg-gray-50 text-center py-14">
+    <h1 class="text-purple-primary text-3xl font-bold mb-2">Nájdi si tu pravú vysokú školu pre teba</h1>
+    <h2 class="text-purple-primary text-lg font-semibold">Hľadaj spomedzi všektých vysokých škôl na Slovensku</h2>
+    <div class="mt-20">
+      <input
+        class="border-2 border-purple-primary focus:outline-none text-purple-primary focus:border-purple-500 focus:ring-border-pink-500 focus:ring-1 rounded-lg p-2 w-1/4"
+        type="text" placeholder="FEI STU" v-model="search" v-debounce:200="searchUpdate"
+        debounce-evets="['click', 'keyup']">
     </div>
+  </div>
 </template>
 
 <script>
@@ -22,7 +25,9 @@ export default {
   },
   methods: {
     searchUpdate() {
-      this.$store.dispatch('searchSchools', this.search)
+      if (this.search.length >= 2) {
+        this.$store.dispatch('searchSchools', this.search)
+      }
     }
   },
 }
