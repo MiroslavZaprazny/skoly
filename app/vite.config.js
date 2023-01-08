b8a155d471a0b026a3d43c1c9dc482d98dfa52cf
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2023-01-08 21:02:35 +0100

    fix

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,10 +4,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 8000,
-    watch: {
-      usePolling: true,
-    },
-  },
+  // server: {
+  //   port: 8000,
+  //   watch: {
+  //     usePolling: true,
+  //   },
+  // },
 });
