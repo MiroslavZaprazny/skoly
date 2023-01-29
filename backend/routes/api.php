375341e888dfc55894a5e05ec5fbdc89cb88c3cb
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2023-01-29 14:56:50 +0100

    Comment likes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CollageController;
-use App\Http\Controllers\RatingController;
-use App\Http\Controllers\RatingLikeController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RatingController;
+use App\Http\Controllers\CollageController;
+use App\Http\Controllers\RatingLikeController;
+use App\Http\Controllers\CommentLikeController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -20,9 +21,13 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::patch('/profile/{user}', [UserController::class, 'update']);
     Route::get('/profile/{user}', [UserController::class, 'show']);
 
+    Route::delete('/rating/{rating}', [RatingController::class, 'destroy']);
+
+    Route::post('/comment/{comment}/like', [CommentLikeController::class, 'create']);
+    Route::delete('/comment/{comment}/unlike', [CommentLikeController::class, 'destroy']);
+
     Route::post('/rating/{rating}/like', [RatingLikeController::class, 'create']);
     Route::delete('/rating/{rating}/unlike', [RatingLikeController::class, 'destroy']);
-    Route::delete('/rating/{rating}', [RatingController::class, 'destroy']);
 
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
